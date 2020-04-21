6a1a3a87dfe4abfb9d954bde439e3225c66bbc2d
vikaskulkarni <vikas.kulkarni@gmail.com>
2020-04-20 20:51:21 -0600

    minification

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,10 +1,10 @@
 const { resolve, join } = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin-legacy");
 const isProduction = process.env.NODE_ENV === "production";
 const dist = "dist";
 const pathsToClean = [`${dist}/*.*`];
@@ -12,7 +12,7 @@ const cleanOptions = {
   root: resolve(__dirname, ".."),
   exclude: [`${dist}/.gitignore`],
   verbose: true,
-  dry: false
+  dry: false,
 };
 const plugins = [
   new webpack.EnvironmentPlugin({ NODE_ENV: "development" }),
@@ -20,28 +20,20 @@ const plugins = [
   new LodashModuleReplacementPlugin(),
   new HtmlWebpackPlugin({
     template: join("client", "index.html"),
-    favicon: join("client", "favicon.ico")
+    favicon: join("client", "favicon.ico"),
   }),
   new webpack.optimize.CommonsChunkPlugin({
     name: "vendor",
-    minChunks: Infinity
+    minChunks: Infinity,
   }),
-  new webpack.NamedModulesPlugin()
+  new webpack.NamedModulesPlugin(),
 ];
 if (isProduction) {
   plugins.push(
     new CopyWebpackPlugin([{ from: "client/wp-i.jpg", to: "" }]),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    })
+    new TerserPlugin()
   );
 } else {
-  plugins.push(
-    new webpack.LoaderOptionsPlugin({
-      debug: true
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  );
+  plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 module.exports = plugins;
--- webpack/rules.js
+++ webpack/rules.js
@@ -3,20 +3,20 @@ const rules = [
   {
     test: /.jsx?$/,
     loader: "babel-loader",
-    exclude: /node_modules/
+    exclude: /node_modules/,
   },
   {
     test: /\.(woff2|woff|ttf|eot|svg)(\?.*$|$)/,
     loader: "file-loader?name=fonts/[name].[ext]",
-    include: [join(__dirname, "src"), join(__dirname, "node_modules")]
+    include: [join(__dirname, "src"), join(__dirname, "node_modules")],
   },
   {
     test: /\.(ico)$/i,
-    loader: "file-loader?name=[name].[ext]"
+    loader: "file-loader?name=[name].[ext]",
   },
   {
     test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-    loader: "url-loader?limit=100000"
-  }
+    loader: "url-loader?limit=100000",
+  },
 ];
 module.exports = rules;
